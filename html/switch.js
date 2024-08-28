892fa612c9647693aa78f6e86f49cec75c0ff9f0
EntentaPL <lukaszdobrowolski04@gmail.com>
2024-08-28 17:06:51 +0200

    remove extension gui

--- html/switch.js
+++
@@ -1,7 +0,0 @@
-import "../modules/jquery.min.js"
-
-$(".refer").on("click", function(){
-    chrome.tabs.create({active: true, url: "https://github.com/EntentaPL/Scratch-Trends/"});
-})
-
-$("#switcher").click();
